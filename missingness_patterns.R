daf2ebd0c85dc3786c801f3cbae48a818b14b4fa
simon <simfont@umich.edu>
2021-09-13 10:14:41 -0400

    hierarchical clustering

--- missingness_patterns.R
+++ missingness_patterns.R
@@ -15,6 +15,32 @@ missingness_patterns = function(df){
 # load data
 load('R_data/subsample/sub_complete_data_impute.RData')
 
-# check patterns
-counts = missingness_patterns(train_data_impute$clean)
-counts = counts %>% arrange()
+# df = train_data_impute
+df = starwars 
+counts = missingness_patterns(df)
+n_groups = 5
+
+library(tidyverse)
+# build tree
+pattern_tree = function(df, n_groups){
+  # observed patterns
+  colnms = colnames(df)
+  patterns = df %>%
+    filter_at(vars(all_of(colnms)), any_vars(is.na(.))) %>%
+    is.na %>% (function(x) !x) %>% as_tibble
+  
+  # counts per pattern
+  counts = patterns %>%
+    group_by_all() %>% summarise(count = n(), .groups="drop")
+  common = counts %>% summarise_all(all)
+  
+  # keep largest
+  large_patterns = counts %>% slice_max(order_by=count, n=n_groups)
+  common$count = sum(counts$count) - sum(large_patterns$count)
+  large_patterns[nrow(large_patterns)+1, ] = common
+  x = large_patterns %>% select(!count)
+  y = large_patterns %>% select(count)
+  
+  return(large_patterns)
+}
+
