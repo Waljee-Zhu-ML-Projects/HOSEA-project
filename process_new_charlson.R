62f72e4d20248cb7b6ad5464d395c1d9649d85a4
pwmacdon <pwmacdon@umich.edu>
2021-09-27 18:32:30 -0400

    script to process new charlson/disease indicators

--- process_new_charlson.R
+++ process_new_charlson.R
@@ -1,49 +1,57 @@
 # new indicators for identifying Charlson scores
 
-# example: COPD
+# helpers and code checking dictionary
+source('R_code/hosea-project/utils_missingcharl.R')
 
-# define the name of the disease as it appears in the full data
-charl_name <- 'copd'
-# define functions to identify ICD9 and 10 codes
-check_code_icd9 <- function(code){
-  as.integer(substr(code,1,2)=='49')
-}
-
-check_code_icd10 <- function(code){
-  as.integer(substr(code,1,3)=='J44')
-}
-
-charl_chunk <- list()
-length(charl_chunk) <- 6
-
+# load data
+alldxs <- list()
+length(alldxs) <- 6
 for(cc in c(1:5,'cx')){
-  print(paste0('For chunk ',chunk))
-  # load data
-  alldxs <- readRDS(paste0('R_data/alldxs',chunk,'_filter.rds'))
-  print('Loaded')
-  # new variables
-  alldxs <- mutate(alldxs,charl9=check_code_icd9(icd9code))
-  if(cc == 'cx'){
-    alldxs <- mutate(alldxs,charl10=check_code_icd10(icd10code))
-  }
-  else{
-    alldxs <- mutate(alldxs,charl10=0)
-  }
-  print('Checked codes')
-  # summarize
-  charl_chunk[[cc]] <- summarize(group_by(alldxs,ID),charl=max(charl9 + charl10))
-  print('Summarized chunk')
+  alldxs[[cc]] <- readRDS(paste0('R_data/alldxs',chunk,'_filter.rds'))
 }
 
-# combine individual chunks
-charl_chunk_rbound <- map_dfr(c(1:5,'cx'),function(ii){charl_chunk[[ii]]})
-print('Bind individual chunks')
-
-charl <- summarize(group_by(charl_chunk_rbound,ID),charl = max(charl))
-colnames(charl)[2] <- charl_name
-print('Combine over chunks')
-
-# save as an rds file
-saveRDS(charl,file=paste0('R_data/charl_',charl_name,'.rds'))
-print('Saved')
-
+# loop over disease names
+for(charl_name in charl_names){
+  print(paste0('For disease ',charl_name,':'))
+  timestamp()
+  
+  # define functions to identify ICD9 and 10 codes
+  check_code_icd9 <- charl_checks[[charl_name]][['icd9']]
+  
+  check_code_icd10 <- charl_checks[[charl_name]][['icd10']]
+  
+  charl_chunk <- list()
+  length(charl_chunk) <- 6
+  
+  for(cc in c(1:5,'cx')){
+    print(paste0('For chunk ',chunk))
+    # new variables
+    alldxs_chunk <- mutate(alldxs[[cc]],charl9=check_code_icd9(icd9code))
+    if(cc == 'cx'){
+      alldxs_chunk <- mutate(alldxs_chunk,charl10=check_code_icd10(icd10code))
+    }
+    else{
+      alldxs_chunk <- mutate(alldxs_chunk,charl10=0)
+    }
+    print('Checked codes')
+    # summarize
+    charl_chunk[[cc]] <- summarize(group_by(alldxs_chunk,ID),charl=max(charl9 + charl10))
+    print('Summarized chunk')
+    
+    # remove chunk
+    rm(alldxs_chunk)
+    gc()
+  }
+  
+  # combine individual chunks
+  charl_chunk_rbound <- map_dfr(c(1:5,'cx'),function(cc){charl_chunk[[cc]]})
+  print('Bind individual chunks')
+  
+  charl <- summarize(group_by(charl_chunk_rbound,ID),charl = max(charl))
+  colnames(charl)[2] <- charl_name
+  print('Combine over chunks')
+  
+  # save as an rds file
+  saveRDS(charl,file=paste0('R_data/charlson_',charl_name,'.rds'))
+  print('Saved')
+}
